2afec7126a9274a719a1ad19e2b7e3ed54e85136
stzups <griffinht@gmail.com>
2021-07-06 11:14:36 -0400

    maybe fix samba

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -5,7 +5,7 @@ apt upgrade -y
 
 # install docker engine
 apt-get update
-apt-get install \
+apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -14,12 +14,14 @@ apt-get install \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
-docker version
+apt-get -y install docker-ce docker-ce-cli containerd.io
 
 # install docker compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# done
+docker version
 docker-compose --version
