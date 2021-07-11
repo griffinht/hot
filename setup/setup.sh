109c1b4d96d9cc4603ed3f372cad2063d98180be
stzups <lemonpickles1558@gmail.com>
2021-07-11 16:51:44 -0400

    clean up setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-# generate ssh key for use with github
-# add the public key (printed with cat) to github (preferably to a single repository as a deploy key)
-ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N "" -C "griffinht@gmail.com" && cat ~/.ssh/id_ed25519.pub
-# after adding the public key to github, clone
-git clone git@github.com:stzups/hot.git && cd hot && chmod -R +x ubuntu && ubuntu/init.sh
+
+# Setup script to be ran manually to clone the Github repo and start the actual init script
+
+mkdir hot \
+&& cd hot \
+&& ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N "" -C "griffinht@gmail.com" \
+&& echo "Administrator public key:" \
+&& cat ~/.ssh/id_ed25519.pub \
+&& echo "Add this key to Github as a deploy key for a single repository" \
+&& read -p "Press any key to continue and clone the Github repository" \
+&& git clone git@github.com:stzups/hot.git . \
+&& chmod +x setup/init.sh \
+&& ./setup/init.sh
