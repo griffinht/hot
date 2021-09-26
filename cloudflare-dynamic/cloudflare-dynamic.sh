1d8cc688f9c445041504d6dd98be9eb18546db06
stzups <griffinht@gmail.com>
2021-09-26 18:48:49 -0400

    update works

--- cloudflare-dynamic/cloudflare-dynamic.sh
+++ cloudflare-dynamic/cloudflare-dynamic.sh
@@ -3,13 +3,40 @@
 # path to token
 token=$(cat "$1")
 zone_name="$2"
+# ip address, or empty to get from https://icanhazip.com
+old_content="0.0.0.0"
+new_content="0.0.0.0"
+# todo if no content then get from somewhere
 
-request() {
+get() {
   curl -sS -X GET "https://api.cloudflare.com/client/v4/$1" -H "Authorization: Bearer $token" -H "Content-Type:application/json"
 }
 
-#zone_id=$(request "zones" \
-zone_id=$(cat response \
-  | jq '.result[]| select(.name == "'"$zone_name"'").id')
+zone_id=$(get "zones" \
+  | jq -r '.result[]| select(.name == "'"$zone_name"'").id')
 
-echo "$zone_id"
+if [ -z "$zone_id" ]; then
+  echo "Zone id for zone_name $zone_name not found"
+  exit 1
+fi;
+echo "Found zone id $zone_id for zone_name $zone_name"
+
+dns_ids=$(get "zones/$zone_id/dns_records" \
+  | jq -r '.result[]| select(.content == "'"$old_content"'").id')
+
+if [ -z "$dns_ids" ]; then
+  echo "No dns records to update"
+  exit 0
+fi;
+
+while IFS= read -r dns_id; do
+  echo "Updating dns_id $dns_id"
+  response=$(curl -sS -X PATCH "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$dns_id" -H "Authorization: Bearer $token" -H "Content-Type:application/json" \
+               --data '{"content":"'"$new_content"'"}')
+  if [ "$(echo "$response" | jq '.success')" != "true" ]; then
+    echo "Error"
+    echo "$response"
+    exit 1
+  fi;
+  echo "Success"
+done <<< "$dns_ids"
