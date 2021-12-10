c0dbd2cb4b9576ad7e78870b972fafc184418224
stzups <griffinht@gmail.com>
2021-12-10 18:14:41 -0500

    rework variables

--- certbot/certbot2.sh
+++ certbot/certbot2.sh
@@ -2,19 +2,11 @@
 
 CLOUDFLARE_API_URL=https://api.cloudflare.com/client/v4/
 
-# check if $CLOUDFLARE_API_TOKEN is set
-# token needs the following for each zone (domain) it can manipulate
-# Zone.Zone:Read
-# Zone.DNS:Edit
-cloudflare() {
-  if [ -z "$CLOUDFLARE_API_TOKEN" ]; then
-    echo "CLOUDFLARE_API_TOKEN not set"
-    exit 1
-  fi;
-}
+
 
 cloudflare_api_get() {
-  CLOUDFLARE_API_PATH="$1"
+  CLOUDFLARE_API_TOKEN="$1"
+  CLOUDFLARE_API_PATH="$2"
   cloudflare
   curl -sS -X GET "$CLOUDFLARE_API_URL$CLOUDFLARE_API_PATH" \
     -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
@@ -22,9 +14,10 @@ cloudflare_api_get() {
 }
 
 cloudflare_api_post() {
-  CLOUDFLARE_API_PATH=$1
-  DATA=$2
-  cloudflare
+  CLOUDFLARE_API_TOKEN="$1"
+  CLOUDFLARE_API_PATH="$2"
+  DATA=$3
+  cloudflare "$CLOUDFLARE_API_TOKEN"
   curl -sS -X POST "$CLOUDFLARE_API_URL$CLOUDFLARE_API_PATH" \
     -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
     -H "Content-Type:application/json" \
@@ -33,9 +26,9 @@ cloudflare_api_post() {
 
 function manual-auth-hook()
 {
-  echo "manual-auth-hook running"
+  CLOUDFLARE_API_TOKEN="$1"
 #
-#  cloudflare_api_get zones > test1
+#  cloudflare_api_get "$CLOUDFLARE_API_TOKEN" zones > test1
 
   ZONE_ID=$(jq -r '.result[]| select(.name == "'"$CERTBOT_DOMAIN"'").id' < test1)
   if [ -z "$ZONE_ID" ]; then
@@ -44,7 +37,7 @@ function manual-auth-hook()
   fi;
   echo "Got zone id $ZONE_ID for domain $CERTBOT_DOMAIN via GET to zones"
 #
-#  cloudflare_api_post "zones/$ZONE_ID/dns_records" '{"type":"TXT","name":"_acme-challenge","content":"'"$CERTBOT_VALIDATION"'","ttl":"1"}' > test2
+#  cloudflare_api_post "$CLOUDFLARE_API_TOKEN" "zones/$ZONE_ID/dns_records" '{"type":"TXT","name":"_acme-challenge","content":"'"$CERTBOT_VALIDATION"'","ttl":"1"}' > test2
   if [ "$(jq -r '.success' < test2)" != "true" ]; then
     echo "Error creating DNS record with type TXT with _acme-challenge = $CERTBOT_VALIDATION via POST to zones/$ZONE_ID/dns_records"
     jq '.errors' < test2
@@ -57,6 +50,7 @@ function manual-auth-hook()
 }
 
 function manual-cleanup-hook() {
+  CLOUDFLARE_API_TOKEN="$1"
   echo "manual-cleanup-hook running"
   echo "cleaning up"
   exit 1
@@ -65,7 +59,20 @@ function manual-cleanup-hook() {
 function certbot-certonly()
 {
   DOMAIN="$1"
+  # token needs the following for each zone (domain) it can manipulate
+  # Zone.Zone:Read
+  # Zone.DNS:Edit
   CLOUDFLARE_API_TOKEN="$2"
+
+  if [ -z "$DOMAIN" ]; then
+    echo "DOMAIN not set"
+    exit 1
+  fi;
+  if [ -z "$CLOUDFLARE_API_TOKEN" ]; then
+    echo "CLOUDFLARE_API_TOKEN not set"
+    echo ""
+    exit 1
+  fi;
 
   certbot certonly \
     --test-cert \
@@ -73,8 +80,8 @@ function certbot-certonly()
     --preferred-challenges dns \
     \
     --manual \
-    --manual-auth-hook "./certbot2.sh manual-auth-hook" \
-    --manual-cleanup-hook "./certbot2.sh manual-cleanup-hook" \
+    --manual-auth-hook "./certbot2.sh manual-auth-hook $CLOUDFLARE_API_TOKEN" \
+    --manual-cleanup-hook "./certbot2.sh manual-cleanup-hook $CLOUDFLARE_API_TOKEN" \
     \
     --noninteractive \
     --register-unsafely-without-email \
