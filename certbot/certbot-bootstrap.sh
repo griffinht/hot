af0763c0861dec47764ee0e99331230324105fba
stzups <griffinht@gmail.com>
2021-12-10 18:05:36 -0500

    start to add cf api

--- certbot/certbot-bootstrap.sh
+++ certbot/certbot-bootstrap.sh
@@ -1,36 +1,3 @@
 #!/bin/bash
 
-CLOUDFLARE_API_TOKEN="ElJVsueh0PWy-asGal1mOSAg08uoqcqMdXoYFEpe"
-CLOUDFLARE_API_URL=https://api.cloudflare.com/client/v4/
-CERTBOT_DOMAIN=griffinht.com
-
-cloudflare_api_get() {
- curl -sS -X GET "$CLOUDFLARE_API_URL""$1" \
-   -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
-   -H "Content-Type:application/json"
-}
-
-cloudflare_api_post() {
- curl -sS -X POST "$CLOUDFLARE_API_URL""$1" \
-  -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
-  -H "Content-Type:application/json" \
-  --data "$2"
-}
-
-cloudflare_api_get zones > test1
-
-ZONE_ID=$(jq -r '.result[]| select(.name == "'"$CERTBOT_DOMAIN"'").id' < test1)
-if [ -z "$ZONE_ID" ]; then
-  echo "Zone id for domain $CERTBOT_DOMAIN not found"
-  exit 1
-fi;
-echo "Found zone id $ZONE_ID for domain $CERTBOT_DOMAIN"
-exit 1
-curl -X POST "$CLOUDFLARE_API_URL""zones/$ZONE_ID/dns_records" \
-     -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
-     -H "Content-Type:application/json" > test1
-
-
-
-
-#./certbot2.sh certbot-certonly "$CLOUDFLARE_API_TOKEN"
+./certbot2.sh certbot-certonly griffinht.com "$CLOUDFLARE_API_TOKEN"
--- certbot/certbot2.sh
+++ certbot/certbot2.sh
@@ -1,8 +1,68 @@
 #!/bin/bash
 
+CLOUDFLARE_API_URL=https://api.cloudflare.com/client/v4/
+
+# check if $CLOUDFLARE_API_URL is set (call this before using $CLOUDFLARE_API_URL)
+cloudflare() {
+  if [ -z "$CLOUDFLARE_API_URL" ]; then
+    echo "CLOUDFLARE_API_URL not set"
+    exit 1
+  fi;
+}
+
+cloudflare_api_get() {
+  CLOUDFLARE_API_PATH="$1"
+  cloudflare
+  curl -sS -X GET "$CLOUDFLARE_API_URL$CLOUDFLARE_API_PATH" \
+    -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
+    -H "Content-Type:application/json"
+}
+
+cloudflare_api_post() {
+  CLOUDFLARE_API_PATH=$1
+  DATA=$2
+  cloudflare
+  curl -sS -X POST "$CLOUDFLARE_API_URL$CLOUDFLARE_API_PATH" \
+    -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
+    -H "Content-Type:application/json" \
+    --data "$DATA"
+}
+
+function manual-auth-hook()
+{
+  echo "manual-auth-hook running"
+#
+#  cloudflare_api_get zones > test1
+
+  ZONE_ID=$(jq -r '.result[]| select(.name == "'"$CERTBOT_DOMAIN"'").id' < test1)
+  if [ -z "$ZONE_ID" ]; then
+    echo "Zone id for domain $CERTBOT_DOMAIN not found via GET to zones"
+    exit 1
+  fi;
+  echo "Got zone id $ZONE_ID for domain $CERTBOT_DOMAIN via GET to zones"
+#
+#  cloudflare_api_post "zones/$ZONE_ID/dns_records" '{"type":"TXT","name":"_acme-challenge","content":"'"$CERTBOT_VALIDATION"'","ttl":"1"}' > test2
+  if [ "$(jq -r '.success' < test2)" != "true" ]; then
+    echo "Error creating DNS record with type TXT with _acme-challenge = $CERTBOT_VALIDATION via POST to zones/$ZONE_ID/dns_records"
+    jq '.errors' < test2
+    exit 1
+  fi
+  DNS_RECORD_ID=$(jq -r '.result.id' < test2)
+  echo "Created DNS record with id $DNS_RECORD_ID with type TXT with _acme-challenge = $CERTBOT_VALIDATION via POST to zones/$ZONE_ID/dns_records"
+
+  exit 1
+}
+
+function manual-cleanup-hook() {
+  echo "manual-cleanup-hook running"
+  echo "cleaning up"
+  exit 1
+}
+
 function certbot-certonly()
 {
   DOMAIN="$1"
+  CLOUDFLARE_API_TOKEN="$2"
 
   certbot certonly \
     --test-cert \
@@ -21,25 +81,6 @@ function certbot-certonly()
     --work-dir test/work \
     --logs-dir test/logs
 
-}
-
-function manual-auth-hook()
-{
-  echo "manual-auth-hook running"
-  echo "$CERTBOT_VALIDATION" > validation
-  sleep 1000000000
-  exit 1
-  curl -X GET "https://api.cloudflare.com/client/v4/user/tokens/verify" \
-       -H "Authorization: Bearer 69kTGmktcokw0ackJP6X6L9DTke0433C-j6REIRg" \
-       -H "Content-Type:application/json"
-# txt record _acme-challenge
-  exit 1
-}
-
-function manual-cleanup-hook() {
-  echo "manual-cleanup-hook running"
-  echo "cleaning up"
-  exit 1
 }
 
 "$@"
