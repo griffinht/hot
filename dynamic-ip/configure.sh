cda98040fa0da69c6582f3efd7f29d09a565888d
stzups <griffinht@gmail.com>
2021-12-11 13:55:21 -0500

    fix multi zones for real

--- dynamic-ip/configure.sh
+++ dynamic-ip/configure.sh
@@ -5,14 +5,23 @@ CLOUDFLARE_API_URL="https://api.cloudflare.com/client/v4/"
 CLOUDFLARE_API_TOKEN="$1"
 CONTENT="$2"
 
-rm -f dynamic-ip/paths
+if [ -z "$CLOUDFLARE_API_TOKEN" ]; then
+  echo "Specify a CLOUDFLARE_API_TOKEN for dynamic-ip"
+  echo "Go to Cloudflare dashboard > Person Icon (top right) > My Profile > API Tokens > Create Token > Custom Custom Token"
+  echo "Token name: hot_dynamic-ip"
+  echo "Permissions: Zone, DNS, Read/Edit"
+  echo "Continue to summary > Create Token > (enter token below)"
+  read -srp "CLOUDFLARE_API_TOKEN: " CLOUDFLARE_API_TOKEN
+  echo
+fi
+if [ -z "$CONTENT" ]; then
+  echo "Specify a CONTENT - current IP address of the DNS records you want to select to keep updated, e.g. 123.123.123.123)"
+  read -rp "CONTENT: " CONTENT
+fi
 
-for ZONE_ID in "${@:3}"; do
+function zone_id() {
+  ZONE_ID="$1"
   echo "Doing $ZONE_ID"
-  # todo read -p
-  if [ -z "$CLOUDFLARE_API_TOKEN" ]; then printf "Specify a CLOUDFLARE_API_TOKEN - go to Cloudflare dashboard > Person Icon (top right) > My Profile > API Tokens > Create Token > Custom Custom Token\nToken name: hot_dynamic-ip\nPermissions: Zone, DNS, Read/Edit (and edit for cloudflare-dynamic.sh)\nContinue to summary > Create Token > (pass given token as first argument)\n"; exit 1; fi
-  if [ -z "$ZONE_ID" ]; then printf "Specify a ZONE_ID - go to Cloudflare dashboard > (your domain) > Overview > API > Zone ID\n"; exit 1; fi
-  if [ -z "$CONTENT" ]; then printf "Specify a CONTENT - current IP address of the DNS records you want to select to keep updated, e.g. 123.123.123.123)\n"; exit 1; fi
 
   RESPONSE=$(curl -sS -X GET \
       -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
@@ -34,4 +43,18 @@ for ZONE_ID in "${@:3}"; do
 
   echo "Success! Saved token to bin/dynamic-ip_cloudflare and dynamic-ip/paths to file"
   cat dynamic-ip/paths
-done
+}
+
+rm -f dynamic-ip/paths
+
+a=${@:3}
+if [ -n "$a" ]; then
+  for ZONE_ID in "${@:3}"; do
+    zone_id "$ZONE_ID"
+  done
+else
+  echo "Specify multiple ZONE_ID - go to Cloudflare dashboard > (your domain) > Overview > API > Zone ID"
+  while read -rp "ZONE_ID: " ZONE_ID; do
+    zone_id "$ZONE_ID"
+  done;
+fi
