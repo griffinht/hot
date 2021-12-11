5b549c198b3b0bf0cc000aed529596c9856e3218
stzups <griffinht@gmail.com>
2021-12-11 02:16:16 -0500

    comments, future wireguard stuff

--- mikrotik/mikrotik.sh
+++ mikrotik/mikrotik.sh
@@ -1,7 +1,10 @@
 ssh user@192.168.0.1
 
+# http with nat hairpin
 #https://help.mikrotik.com/docs/display/ROS/NAT#NAT-HairpinNAT
 #https://forum.mikrotik.com/viewtopic.php?t=172380
-
 ip firewall nat add chain=dstnat action=dst-nat dst-address=98.21.46.184 dst-port=443 to-addresses=192.168.0.254 protocol=tcp
 ip firewall nat add action=masquerade chain=srcnat dst-address=192.168.0.254 out-interface-list=LAN protocol=tcp src-address=192.168.0.0/24
+
+# wireguard (on port 80 to circumvent port blocking)
+ip firewall nat add chain=dstnat action=dst-nat dst-address=98.21.46.184 dst-port=80 to-addresses=192.168.0.254 protocol=tcp
