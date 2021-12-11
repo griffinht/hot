c8e891503d67576f079a7fcc763ae5bae2753719
stzups <griffinht@gmail.com>
2021-12-11 01:39:52 -0500

    fix multi domain

--- dynamic-ip/configure.sh
+++ dynamic-ip/configure.sh
@@ -3,30 +3,35 @@
 CLOUDFLARE_API_URL="https://api.cloudflare.com/client/v4/"
 
 CLOUDFLARE_API_TOKEN="$1"
-ZONE_ID="$2"
-CONTENT="$3"
-# todo read -p
-if [ -z "$CLOUDFLARE_API_TOKEN" ]; then printf "Specify a CLOUDFLARE_API_TOKEN - go to Cloudflare dashboard > Person Icon (top right) > My Profile > API Tokens > Create Token > Custom Custom Token\nToken name: hot_dynamic-ip\nPermissions: Zone, DNS, Read/Edit (and edit for cloudflare-dynamic.sh)\nContinue to summary > Create Token > (pass given token as first argument)\n"; exit 1; fi
-if [ -z "$ZONE_ID" ]; then printf "Specify a ZONE_ID - go to Cloudflare dashboard > (your domain) > Overview > API > Zone ID\n"; exit 1; fi
-if [ -z "$CONTENT" ]; then printf "Specify a CONTENT - current IP address of the DNS records you want to select to keep updated, e.g. 123.123.123.123)\n"; exit 1; fi
+CONTENT="$2"
 
-RESPONSE=$(curl -sS -X GET \
-    -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
-    -H "Content-Type:application/json" \
-    "$CLOUDFLARE_API_URL""zones/$ZONE_ID/dns_records")
+rm -f dynamic-ip/ids
 
-if [ "$(echo "$RESPONSE" | jq '.success')" != "true" ]; then
-  echo "Error"
-  echo "$RESPONSE" | jq
-  exit 1
-fi;
+for ZONE_ID in "${@:3}"; do
+  echo "Doing $ZONE_ID"
+  # todo read -p
+  if [ -z "$CLOUDFLARE_API_TOKEN" ]; then printf "Specify a CLOUDFLARE_API_TOKEN - go to Cloudflare dashboard > Person Icon (top right) > My Profile > API Tokens > Create Token > Custom Custom Token\nToken name: hot_dynamic-ip\nPermissions: Zone, DNS, Read/Edit (and edit for cloudflare-dynamic.sh)\nContinue to summary > Create Token > (pass given token as first argument)\n"; exit 1; fi
+  if [ -z "$ZONE_ID" ]; then printf "Specify a ZONE_ID - go to Cloudflare dashboard > (your domain) > Overview > API > Zone ID\n"; exit 1; fi
+  if [ -z "$CONTENT" ]; then printf "Specify a CONTENT - current IP address of the DNS records you want to select to keep updated, e.g. 123.123.123.123)\n"; exit 1; fi
 
-IDS="$(echo "$RESPONSE" | jq -r '.result[]| select(.content == "'"$CONTENT"'").id')"
-while IFS= read -r ID; do
-  echo "Adding $ID"
-  echo "zones/$ZONE_ID/dns_records/$ID" >> dynamic-ip/ids
-done <<< "$IDS"
-echo "$CLOUDFLARE_API_TOKEN" > bin/dynamic-ip_cloudflare
+  RESPONSE=$(curl -sS -X GET \
+      -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
+      -H "Content-Type:application/json" \
+      "$CLOUDFLARE_API_URL""zones/$ZONE_ID/dns_records")
 
-echo "Success! Saved token to bin/dynamic-ip_cloudflare and dynamic-ip/ids to file"
-cat dynamic-ip/ids
+  if [ "$(echo "$RESPONSE" | jq '.success')" != "true" ]; then
+    echo "Error"
+    echo "$RESPONSE" | jq
+    exit 1
+  fi;
+
+  IDS="$(echo "$RESPONSE" | jq -r '.result[]| select(.content == "'"$CONTENT"'").id')"
+  while IFS= read -r ID; do
+    echo "Adding $ID"
+    echo "zones/$ZONE_ID/dns_records/$ID" >> dynamic-ip/ids
+  done <<< "$IDS"
+  echo "$CLOUDFLARE_API_TOKEN" > bin/dynamic-ip_cloudflare
+
+  echo "Success! Saved token to bin/dynamic-ip_cloudflare and dynamic-ip/ids to file"
+  cat dynamic-ip/ids
+done
