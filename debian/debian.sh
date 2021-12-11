dbf71a015b227c338a0a9a0c2169ae8a7adb11e1
stzups <griffinht@gmail.com>
2021-12-11 17:28:26 -0500

    add md5

--- debian/debian.sh
+++ debian/debian.sh
@@ -3,23 +3,39 @@ set -e
 
 curl https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-11.1.0-amd64-netinst.iso -C - -Lo debian.iso
 
-sudo apt-get -y install xorriso
-
-if [ -d isofiles ]; then
-  chmod +w -R isofiles
-  rm -r isofiles
-fi
+sudo apt-get -y install xorriso genisoimage
 
 xorriso -osirrox on -indev debian.iso  -extract / isofiles/
-exit 1
+#rm debian.iso
 #
 chmod +w -R isofiles/install.amd/
 gunzip isofiles/install.amd/initrd.gz
 
-cpio -H newc -o -A -F isofiles/install.amd/initrd < preseed
+#echo "preseed" | cpio -H newc -o -A -F isofiles/install.amd/initrd
 
 gzip isofiles/install.amd/initrd
 chmod -w -R isofiles/install.amd/
+
+#md5
+(
+cd isofiles/
+chmod a+w md5sum.txt
+md5sum `find -follow -type f` > md5sum.txt
+chmod a-w md5sum.txt
+)
 #
 chmod a+w isofiles/isolinux/isolinux.bin
-genisoimage -r -J -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o debian-10-unattended.iso isofiles
+genisoimage -r -J -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o debian-unattended.iso isofiles
+# clean
+chmod +w -R isofiles
+rm -r isofiles
+
+lsblk
+read -rp "Plug in device, then press enter"
+lsblk
+read -rp "Which device? (/dev/???): " DEVICE
+sudo cp debian-unattended.iso "$DEVICE"
+echo "blocking until finished..."
+sync
+echo "copy complete!"
+rm debian-unattended.iso
