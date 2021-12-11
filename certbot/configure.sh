ee2abbd6d4225d98df0609ce62056b0b81699d9e
stzups <griffinht@gmail.com>
2021-12-11 13:32:18 -0500

    add prompt

--- certbot/configure.sh
+++ certbot/configure.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 CLOUDFLARE_API_TOKEN="$1"
-if [ -z "$CLOUDFLARE_API_TOKEN" ]; then printf "Specify a CLOUDFLARE_API_TOKEN - go to Cloudflare dashboard > Person Icon (top right) > My Profile > API Tokens > Create Token > Custom Custom Token\nToken name: hot_certbot\nPermissions: Zone, DNS, Read/Edit (and edit for cloudflare-dynamic.sh)\nContinue to summary > Create Token > (pass given token as first argument)\n"; exit 1; fi
+if [ -z "$CLOUDFLARE_API_TOKEN" ]; then
+  echo "Specify a CLOUDFLARE_API_TOKEN for certbot"
+  echo "Go to Cloudflare dashboard > Person Icon (top right) > My Profile > API Tokens > Create Token > Custom Custom Token"
+  echo "Token name: hot_certbot"
+  echo "Permissions: Zone, DNS, Read/Edit (and edit for cloudflare-dynamic.sh)"
+  echo "Continue to summary > Create Token > (pass given token as first argument)"
+  read -rp "CLOUDFLARE_API_TOKEN: " CLOUDFLARE_API_TOKEN
+fi
 echo "dns_cloudflare_api_token = $CLOUDFLARE_API_TOKEN" > bin/dynamic-ip_cloudflare
