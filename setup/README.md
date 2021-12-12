3fc4694265d2c24ea785c58fe6bb177c68b770c6
stzups <griffinht@gmail.com>
2021-12-12 18:27:04 -0500

    start to add hot.sh and readme

---
+++ setup/README.md
@@ -0,0 +1,6 @@
+- docker.sh
+
+
+Github > repository > `Settings` > `Deploy Keys` > `Add deploy key`
+Title: hot-desktop
+Key: `ssh-keygen -t ed25519 -C "griffinht@gmail.com"`
---
+++ setup/hot.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+# hot
+apt-get install -y git
+useradd -m -s /bin/bash hot
+su - hot << EOF
+# generate a deploy key for GitHub
+ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N "" -C "griffinht@gmail.com"
+echo "public key:"
+cat ~/.ssh/id_ed25519.pub
+echo "Add this key to GitHub as a deploy key for this repository"
+read -rp "Press enter to continue and clone repository"
+
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+git clone git@github.com:stzups/hot.git
+EOF
+
+# rootless docker
+apt-get install -y dbus-user-session slirp4netns
+# todo restart here
+loginctl enable-linger hot
+# make sure environment variables are escaped! https://stackoverflow.com/a/27921346/11975214
+systemd-run --uid=docker-user --pipe /bin/bash << 'EOF'
+# https://unix.stackexchange.com/a/657714/480971 :)
+export XDG_RUNTIME_DIR=/run/user/$UID
+curl -fsSL https://get.docker.com/rootless | sh
+#optional?
+#export PATH=/home/$(whoami)/bin:$PATH
+export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
+systemctl --user start docker
+printf "export XDG_RUNTIME_DIR=/run/user/\$UID\nexport DOCKER_HOST=unix://\$XDG_RUNTIME_DIR/docker.sock\n" >> ~/.bashrc
+
+docker version
+docker-compose version
+
+EOF
+
+sysctl -w net.ipv4.ip_unprivileged_port_start=123
