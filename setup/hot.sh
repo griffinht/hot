cfdd5711e179d189898906da1d30d79efe31aad0
stzups <griffinht@gmail.com>
2021-12-12 18:30:29 -0500

    improve formatting

--- setup/hot.sh
+++ setup/hot.sh
@@ -7,11 +7,14 @@ useradd -m -s /bin/bash hot
 su - hot << EOF
 # generate a deploy key for GitHub
 ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N "" -C "griffinht@gmail.com"
-echo "public key:"
+echo
+echo "Title: hot-desktop"
+echo "Key: "
 cat ~/.ssh/id_ed25519.pub
+echo
 echo "Add this key to GitHub as a deploy key for this repository"
 read -rp "Press enter to continue and clone repository"
-
+#todo
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 git clone git@github.com:stzups/hot.git
 EOF
