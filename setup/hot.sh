f21d13a4ff50fb0a7345ffc35f6806cf3bbb9b61
stzups <griffinht@gmail.com>
2021-12-23 17:34:01 -0500

    simplify

--- setup/hot.sh
+++ setup/hot.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
 
+./scripts/docker.sh
+
 useradd -m -s /bin/bash docker-user
-su - docker-user << 'EOF'
-mkdir -p ~/.ssh
-cat /home/admin/.ssh/authorized_keys > ~/.ssh/authorized_keys
-EOF
+
+./scripts/rootless-docker.sh docker-user
+
+echo "net.ipv4.ip_unprivileged_port_start = 0" > /etc/sysctl.d/hot.conf
+sudo sysctl --system
+
+
+
+
+
 
 # run from diff host!
 ssh -T docker-user@192.168.0.5 << 'EOF'
--- setup/scripts/sysctl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "net.ipv4.ip_unprivileged_port_start = 0" > /etc/sysctl.d/hot.conf
-sudo sysctl --system
