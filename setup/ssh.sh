0ae670fb9b19317eedd977ffa3bc03a32bba202e
stzups <griffinht@gmail.com>
2021-12-23 18:03:18 -0500

    move scripts, add ssh loader

---
+++ setup/ssh.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+HOST="$1"
+if [ -z "$HOST" ]; then
+  echo specify a host
+  exit 1
+fi
+
+temp=$(mktemp)
+tar -cf "$temp" ./setup
+scp "$temp" "$HOST":setup.tar
+
+ssh "$HOST" /bin/bash << EOF
+tar -xf setup.tar
+rm setup.tar
+./setup/setup.sh
+rm -r ./setup
+EOF
