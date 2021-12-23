0bcf2569097e7de80b0d2e2a960cec0f56cadbd1
stzups <griffinht@gmail.com>
2021-12-23 14:40:37 -0500

    improve rootless-docker.sh

--- setup/hot.sh
+++ setup/hot.sh
@@ -7,16 +7,6 @@ mkdir -p ~/.ssh
 cat /home/admin/.ssh/authorized_keys > ~/.ssh/authorized_keys
 EOF
 
-# rootless docker
-apt-get install -y uidmap
-# not needed
-#apt-get install -y slirp4netns
-
-# make sure environment variables are escaped! https://stackoverflow.com/a/27921346/11975214
-# machinectl
-#apt-get install -y systemd-container
-
-
 # run from diff host!
 ssh -T docker-user@192.168.0.5 << 'EOF'
 #curl -fsSL https://get.docker.com/rootless | sh
@@ -25,32 +15,6 @@ printf "export DOCKER_HOST=unix://\$XDG_RUNTIME_DIR/docker.sock\n" >> ~/.bashrc
 EOF
 docker context create hot-desktop --docker host=ssh://docker-user@192.168.0.5
 
-#todo this or that
-systemd-run --uid=docker-user --pipe /bin/bash << 'EOF'
-## fix XDG_RUNTIME_DIR
-# https://unix.stackexchange.com/a/657714/480971 :)
-export XDG_RUNTIME_DIR=/run/user/$UID
-printf "export XDG_RUNTIME_DIR=/run/user/\$UID\n" >> ~/.bashrc
-
-## rootless docker (needs XDG_RUNTIME_DIR)
-# dockerd-rootless-setuptool.sh install ????
-curl -fsSL https://get.docker.com/rootless | sh
-
-## fix DOCKER_HOST
-#optional?
-#export PATH=/home/$(whoami)/bin:$PATH
-export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
-printf "export DOCKER_HOST=unix://\$XDG_RUNTIME_DIR/docker.sock\n" >> ~/.bashrc
-
-## start docker - todo necessary???
-#systemctl --user start docker
-
-docker version
-docker-compose version
-
-EOF
-loginctl enable-linger docker-user
-
 # hot
-sysctl -w net.ipv4.ip_unprivileged_port_start=123
-echo "net.ipv4.ip_unprivileged_port_start=123" > /etc/sysctl.d/docker.conf
+sudo sysctl --system
+echo "net.ipv4.ip_unprivileged_port_start=0" > /etc/sysctl.d/hot.conf
--- setup/scripts/rootless-docker.sh
+++ setup/scripts/rootless-docker.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
 
-
-apt-get install -y uidmap
-
-if dpkg -l slirp4netns; then
-  echo warning: manually installing optional package slirp4netns because it was not already installed
-  apt-get install -y slirp4netns
+# usage
+# rootless-docker.sh $USER
+USER="$1"
+if [ -z "$USER" ]; then
+  echo specify a user
+  exit 1
 fi
 
+# https://docs.docker.com/engine/security/rootless/
+
+# system dependencies
 if dpkg -l dbus-user-session; then
   echo error: dbus-user-session not installed
   echo this should be installed manually, and might require a restart
   exit 1;
 fi
+
+apt-get install -y uidmap
+
+# https://docs.docker.com/engine/security/rootless/#networking-errors
+if dpkg -l slirp4netns; then
+  echo warning: manually installing optional package slirp4netns because it was not already installed
+  apt-get install -y slirp4netns
+fi
+
+# system setup
+# https://unix.stackexchange.com/a/657714/480971
+loginctl enable-linger "$USER"
+
+# user install
+#systemd-run --uid="$USER" --pipe /bin/bash << 'EOF'
+su - docker-user << 'EOF'
+export XDG_RUNTIME_DIR=/run/user/$UID
+dockerd-rootless-setuptool.sh install
+EOF
+
+# user setup
+#systemd-run --uid="$USER" --pipe /bin/bash << 'EOF'
+su - docker-user << 'EOF'
+# add environment variables to beginning of .bashrc (before non-interactive check)
+# https://superuser.com/a/246841/1252579
+temp=$(mktemp)
+printf '# setup/rootless-docker.sh\nexport XDG_RUNTIME_DIR=/run/user/$UID\nexport DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock\n' | cat - ~/.bashrc > $temp && mv $temp ~/.bashrc
+EOF
