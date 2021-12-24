584a41fddc59ce49e3205f0644d19d74dc1cbff8
stzups <griffinht@gmail.com>
2021-12-24 16:42:15 -0500

    fix source ip

--- setup/setup/hot.sh
+++ setup/setup/hot.sh
@@ -15,6 +15,16 @@ mkdir -p ~/.ssh/
 cat "$(pwd)"/authorized_keys > ~/.ssh/authorized_keys
 EOF
 
+# https://docs.docker.com/engine/security/rootless/#networking-errors
+# fix for docker run -p does not propagate source IP addresses
+# decreases throughput according to docs
+#todo test might require reload
+mkdir -p ~/.config/systemd/user/docker.service.d/
+cat << EOF > ~/.config/systemd/user/docker.service.d/override.conf
+[Service]
+Environment="DOCKERD_ROOTLESS_ROOTLESSKIT_PORT_DRIVER=slirp4netns"
+EOF
+
 ./scripts/rootless-docker.sh "$USER"
 
 echo "net.ipv4.ip_unprivileged_port_start = 0" > /etc/sysctl.d/hot.conf
