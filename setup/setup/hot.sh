d903f8e01cb43965b00c1f0b67d32c20dc3b88b9
stzups <griffinht@gmail.com>
2021-12-23 19:45:55 -0500

    make idempotent

--- setup/setup/hot.sh
+++ setup/setup/hot.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
 
+USER="docker-user"
+
 ./scripts/docker.sh
 
-useradd -m -s /bin/bash docker-user
+if id "$USER" > /dev/null; then
+  echo warning: user "$USER" already exists
+else
+  useradd -m -s /bin/bash "$USER"
+fi
 
-./scripts/rootless-docker.sh docker-user
+./scripts/rootless-docker.sh "$USER"
 
 echo "net.ipv4.ip_unprivileged_port_start = 0" > /etc/sysctl.d/hot.conf
 sudo sysctl --system
