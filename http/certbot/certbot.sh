1ac7c2a2fb6499212694f454e254fcebaf1fdd06
stzups <griffinht@gmail.com>
2021-12-24 19:10:13 -0500

    reorganize and update ssl certs

--- http/certbot/certbot.sh
+++ http/certbot/certbot.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # todo remove --test-cert on 12/31
+# todo add aws griffin.ht
 certbot certonly \
   --test-cert \
   -d griffinht.com \
+  -d stzups.net \
   -d scribbleshare.com \
+  -d realgmoney.com \
+  -d lemonpickles.net \
   \
   --noninteractive \
   --register-unsafely-without-email \
