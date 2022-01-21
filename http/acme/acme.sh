f6fe2f287a88653c9567ded58c878b765cea19a7
stzups <griffinht@gmail.com>
2022-01-21 18:31:45 -0500

    remove extra domains

--- http/acme/acme.sh
+++ http/acme/acme.sh
@@ -7,7 +7,4 @@ acme.sh \
   -d 'griffinht.com' --dns dns_cf \
   -d '*.griffinht.com' --dns dns_cf \
   -d 'stzups.net' --dns dns_cf \
-  -d 'scribbleshare.com' --dns dns_cf \
-  -d 'realgmoney.com' --dns dns_cf \
-  -d 'lemonpickles.net' --dns dns_cf \
   -d 'griffin.ht' --dns dns_aws
