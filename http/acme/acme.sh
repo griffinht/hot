2e4a00bd84d7348b4d3253383285071b5ac18cde
stzups <griffinht@gmail.com>
2022-01-21 17:08:05 -0500

    fix ssl wildcard cert

--- http/acme/acme.sh
+++ http/acme/acme.sh
@@ -4,9 +4,10 @@ acme.sh \
   --cert-home /certificates \
   --server letsencrypt \
   --issue \
-  -d griffinht.com --dns dns_cf \
-  -d stzups.net --dns dns_cf \
-  -d scribbleshare.com --dns dns_cf \
-  -d realgmoney.com --dns dns_cf \
-  -d lemonpickles.net --dns dns_cf \
-  -d griffin.ht --dns dns_aws
+  -d 'griffinht.com' --dns dns_cf \
+  -d '*.griffinht.com' --dns dns_cf \
+  -d 'stzups.net' --dns dns_cf \
+  -d 'scribbleshare.com' --dns dns_cf \
+  -d 'realgmoney.com' --dns dns_cf \
+  -d 'lemonpickles.net' --dns dns_cf \
+  -d 'griffin.ht' --dns dns_aws
