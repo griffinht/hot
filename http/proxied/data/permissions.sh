97679161ce3c8090266e5684fd63d28c8fd35256
stzups <griffinht@gmail.com>
2022-01-22 17:05:38 -0500

    change volume structure

--- http/proxied/data/permissions.sh
+++ http/proxied/data/permissions.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 cd /data
-mkdir public
 (
+mkdir public
 cd public
 mkdir griffin
 )
-mkdir wireguard
+(
+mkdir docker
+cd docker
+mkdir wireguard_wg-access-server-data
+mkdir http_certificates
+mkdir http_acme
+)
