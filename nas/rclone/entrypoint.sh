ecd24861443d6ab094ce7ff7e0159e080522833d
stzups <griffinht@gmail.com>
2022-01-23 14:17:41 -0500

    improve rclone sync

--- nas/rclone/entrypoint.sh
+++ nas/rclone/entrypoint.sh
@@ -3,17 +3,24 @@
 B2_ACCOUNT="$RCLONE_B2_ACCOUNT"
 B2_KEY="$RCLONE_B2_KEY"
 SOURCE='/backup'
+CACHE='/rclone/cache'
 REMOTE='hot'
 
+echo creating config
 rclone config create "$REMOTE" b2 \
   --b2-account "$B2_ACCOUNT"\
   --b2-key "$B2_KEY"
 
 sync() {
-  rclone sync --progress "$SOURCE" "$REMOTE":
+  rclone sync \
+    --progress \
+    --cache-dir "$CACHE" \
+    "$SOURCE" "$REMOTE":
 }
 
 while true; do
+  echo 'syncing'
   sync
-  sleep 15s
+  echo 'done'
+  sleep '15s'
 done
