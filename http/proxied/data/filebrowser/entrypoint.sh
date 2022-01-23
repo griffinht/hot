03111ef239d8ae3077de05027ee90766753bc3b1
stzups <griffinht@gmail.com>
2022-01-22 19:16:58 -0500

    make filebrowser stateless

--- http/proxied/data/filebrowser/entrypoint.sh
+++ http/proxied/data/filebrowser/entrypoint.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-if [ ! -f "/data/filebrowser.db" ]; then
-  /filebrowser -d /data/filebrowser.db config init
-fi
+/filebrowser -d /data/filebrowser.db config init
 
 /filebrowser -d /data/filebrowser.db config set \
     --baseurl "/filebrowser" \
     --address "" \
+    --port "80" \
     --root "public" \
     --auth.method "noauth"
 
-/filebrowser -d /data/filebrowser.db users update 1 \
-    --username "user" \
+/filebrowser -d /data/filebrowser.db users add user password \
     --perm.admin=false \
     --perm.execute=false \
     --perm.share=false \
