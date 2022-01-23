467f8304ddadf3fdaa9bdc8d81e26f722069dfb7
stzups <griffinht@gmail.com>
2022-01-23 10:17:17 -0500

    fix borg init script

--- nas/borg/entrypoint.sh
+++ nas/borg/entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
-echo hello
-sleep 100000
+REPOSITORY='/borg'
+BACKUP='/data'
+
+echo checking "$REPOSITORY"
+if [ "$(ls -A $REPOSITORY)" ]; then
+  echo "$REPOSITORY" is empty, init new borg repository
+  borg init --encryption=none /borg
+fi
+
+backup() {
+  borg create --stats "$REPOSITORY" "$BACKUP"
+}
+
+while true; do
+  backup
+  sleep 100
+done
