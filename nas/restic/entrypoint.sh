1d3ae9a711fb74e81634e3b3a9d822bd7d7b351d
stzups <griffinht@gmail.com>
2022-01-23 15:55:00 -0500

    improve script

--- nas/restic/entrypoint.sh
+++ nas/restic/entrypoint.sh
@@ -1,23 +1,45 @@
 #!/bin/sh
+set -e
 
 export B2_ACCOUNT_ID="$RESTIC_B2_ID"
 export B2_ACCOUNT_KEY="$RESTIC_B2_KEY"
 BACKUP='/data/public'
 BUCKET='hot-griffinht-com'
 REPOSITORY='hot'
+CACHE='/restic/cache'
 PASSWORD="$RESTIC_PASSWORD"
 
-echo "$PASSWORD" | restic -r b2:"$BUCKET":"$REPOSITORY" init
-
-backup() {
-  echo "$PASSWORD" | restic -r b2:"$BUCKET":"$REPOSITORY" \
-    --verbose \
-    backup \
-    "$BACKUP"
+check() {
+  echo "$PASSWORD" | restic \
+    -r b2:"$BUCKET":dfg \
+    snapshots
 }
 
-sleep 10000
+init() {
+  echo "$PASSWORD" | restic \
+    -r b2:"$BUCKET":"$REPOSITORY" \
+    init
+}
+
+echo 'checking for existing repository'
+if ! check; then
+  echo 'repository does not exist, initializing'
+  init
+fi
+echo 'done'
+
+backup() {
+  echo "$PASSWORD" | restic \
+    -r b2:"$BUCKET":"$REPOSITORY" \
+    --cache-dir "$CACHE" \
+    --cleanup-cache \
+    --verbose \
+    backup "$BACKUP"
+}
+
 while true; do
+  echo 'backing up'
+  date +%Y-%m-%dT%T
   backup
   sleep 15s
 done
