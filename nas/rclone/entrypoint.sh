3ea789d8871742b3c36a99423067c263b9209809
stzups <griffinht@gmail.com>
2022-01-23 13:55:27 -0500

    add rclone environment variables and b

--- nas/rclone/entrypoint.sh
+++ nas/rclone/entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
-echo hi
-sleep 1000
+B2_ACCOUNT="$RCLONE_B2_ACCOUNT"
+B2_KEY="$RCLONE_B2_KEY"
+SOURCE='/backup'
+REMOTE='hot'
+
+rclone config create "$REMOTE" b2 \
+  --b2-account "$B2_ACCOUNT"\
+  --b2-key "$B2_KEY"
+
+sync() {
+  rclone sync --progress "$SOURCE" "$REMOTE":
+}
+
+while true; do
+  sync
+  sleep 15s
+done
