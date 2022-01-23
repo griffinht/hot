756c4208170be6f34b26448a78206ef4e138ff08
stzups <griffinht@gmail.com>
2022-01-23 15:40:30 -0500

    add restic script

--- nas/restic/entrypoint.sh
+++ nas/restic/entrypoint.sh
@@ -1,1 +1,23 @@
 #!/bin/sh
+
+export B2_ACCOUNT_ID="$RESTIC_B2_ID"
+export B2_ACCOUNT_KEY="$RESTIC_B2_KEY"
+BACKUP='/data/public'
+BUCKET='hot-griffinht-com'
+REPOSITORY='hot'
+PASSWORD='password'
+
+echo "$PASSWORD" | restic -r b2:"$BUCKET":"$REPOSITORY" init
+
+backup() {
+  echo "$PASSWORD" | restic -r b2:"$BUCKET":"$REPOSITORY" \
+    --verbose \
+    backup \
+    "$BACKUP"
+}
+
+sleep 10000
+while true; do
+  backup
+  sleep 15s
+done
