139b3c70c072c56e1497413d33e3cbc96a603f11
stzups <griffinht@gmail.com>
2022-01-30 15:40:38 -0500

    move wireguard volume

---
+++ setup/setup/scripts/docker-volumes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker volume create --driver local --opt type=nfs --opt o=nfsvers=4,addr=192.168.0.5,rw --opt device=:/docker/http_acme http_acme
+docker volume create --driver local --opt type=nfs --opt o=nfsvers=4,addr=192.168.0.5,rw --opt device=:/docker/http_certificates http_certificates
+docker volume create --driver local --opt type=nfs --opt o=nfsvers=4,addr=192.168.0.5,rw --opt device=:/docker/wireguard_wg-access-server-data wireguard_wg-access-server-data
+docker volume create --driver local --opt type=nfs --opt o=nfsvers=4,addr=192.168.0.5,rw --opt device=:/docker/data_filebrowser data_filebrowser
