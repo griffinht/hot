85946f810133b9e597039d97d3d052423ada97fd
griffinht <git@griffinht.com>
2022-03-23 20:31:33 -0400

    fix mc server

--- http/proxied/minecraft/README.md
+++ http/proxied/minecraft/README.md
@@ -1,7 +1,20 @@
-```
-docker compose up
-```
+# Minecraft Docker image
+Extremely lightweight PurpurMC minecraft server with a `ttyd` web interface for `rcon-cli` for server administration.
 
-```
-docker exec -it $(docker ps | grep minecraft_minecraft | cut -c -12) rcon-cli
-```
+### Volumes
+- `/minecraft`
+
+### Ports
+- `25565` (should be exposed)
+- `80` (should not be exposed, used for `ttyd` web interface for `rcon-cli`)
+
+### Environment variables
+- `WEB`
+    - default unset
+    - set to blank or any value to start the `ttyd` web interface for `rcon-cli`
+- `WEB_BASE_PATH`
+    - default `/`
+    - used for putting the web interface behind a reverse proxy
+- `WEB_PORT`
+    - default `80`
+    - port for web interface
--- http/proxied/minecraft/minecraft/entrypoint.sh
+++ http/proxied/minecraft/minecraft/entrypoint.sh
@@ -1,21 +1,18 @@
-ttyd --base-path /minecraft --port 80 rcon-cli &
+#!/bin/sh
 
-function backup() {
-  while true; do
-    mkdir -p backups
-    echo making backup
-    tar -cz world world_nether world_the_end > backups/"$(date +%F_%T)".tar.gz
-    echo pruning backups
-    echo before:
-    ls backups
-    find backups -type f -mtime +1 -exec rm {} \;
-    echo after:
-    ls backups
-    sleep 1h
-  done
+function web {
+    if [ -z "$WEB_BASE_PATH" ]; then WEB_BASE_PATH='/'; fi
+    if [ -z "$WEB_PORT" ]; then WEB_PORT='80'; fi
+    
+    echo "starting ttyd in background on WEB_PORT=$WEB_PORT with WEB_BASE_PATH=$WEB_BASE_PATH"
+
+    # run ttyd in background
+    ttyd --base-path "$WEB_BASE_PATH" --port "$WEB_PORT" rcon-cli &
 }
-backup &
 
+if [ -z "$WEB" ]; then web; fi
+
+# run server
 java \
 -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 \
- -jar /server.jar nogui
+ -jar /usr/local/bin/server.jar nogui
