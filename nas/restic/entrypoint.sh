e5ddcf27b5abf81717388f5901a9f1fa33acdd58
griffinht <git@griffinht.com>
2022-05-10 08:31:12 -0400

    Fix hostname changing everytime, now restic uses parent snapshot

--- nas/restic/entrypoint.sh
+++ nas/restic/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 BACKUP='/data/public/docker'
 BACKUP2='/data/public/backup'
+HOST='f3fcbdab57b4'
 
 check() {
   restic snapshots
@@ -25,10 +26,12 @@ backup() {
   restic \
     --cleanup-cache \
     --verbose \
+    --host "$HOST" \
     backup "$BACKUP"
   restic \
     --cleanup-cache \
     --verbose \
+    --host "$HOST" \
     backup "$BACKUP2"
   restic forget --prune --keep-daily 3 --keep-weekly 1 --keep-monthly 1
 }
