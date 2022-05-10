6396271cab23ade6b3434d641ff4072d82ed543c
griffinht <git@griffinht.com>
2022-05-10 08:34:44 -0400

    Set hostname in docker compose

--- nas/restic/entrypoint.sh
+++ nas/restic/entrypoint.sh
@@ -3,7 +3,6 @@ set -e
 
 BACKUP='/data/public/docker'
 BACKUP2='/data/public/backup'
-HOST='f3fcbdab57b4'
 
 check() {
   restic snapshots
@@ -26,12 +25,10 @@ backup() {
   restic \
     --cleanup-cache \
     --verbose \
-    --host "$HOST" \
     backup "$BACKUP"
   restic \
     --cleanup-cache \
     --verbose \
-    --host "$HOST" \
     backup "$BACKUP2"
   restic forget --prune --keep-daily 3 --keep-weekly 1 --keep-monthly 1
 }
