59ad96f53fb4389b78278f66b60492338f10db6a
griffinht <git@griffinht.com>
2022-05-25 17:26:35 -0400

    Update scribe

--- http/proxied/scribe/build.sh
+++ http/proxied/scribe/build.sh
@@ -3,11 +3,11 @@
 DIR="$(mktemp -d)"
 cd "$DIR" || exit 1
 
-GIT_REF='defec9319e7e42ac72ffb3674f147f1a4ea92eb4'
-SHA256='cdbd5a3e841aeeceec43f85afed602f22b0e4dbe37ca0fbc6bbedd0b49e6fd16'
+GIT_REF='1dcded9153c482553538fdf9a4f80e7d9361b19c'
+SHA256='eea6614d566aebe22f575722f967a6779ff13715b47261f0023d7fda1d2eff90  tar.gz'
 
 if ! (curl "https://git.sr.ht/~edwardloveall/scribe/archive/$GIT_REF.tar.gz" > tar.gz \
-    && echo "$SHA256  tar.gz" | sha256sum -c \
+    && echo "$SHA256" | sha256sum -c \
     && tar -xz --strip-components=1 < tar.gz \
     && rm tar.gz); then
     echo failed to download and verify source
