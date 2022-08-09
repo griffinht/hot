9475ac10fc182d2f0f3b837f5030552c7a8a1f06
griffinht <git@griffinht.com>
2022-08-09 13:20:22 -0400

    remove http_passwd volume
    this volume is no longer necessary now that autheilia handles auth

--- infrastructure/setup/setup/scripts/docker-volumes.sh
+++ infrastructure/setup/setup/scripts/docker-volumes.sh
@@ -3,7 +3,6 @@
 # todo remove prefix http_
 docker volume create --driver local --opt type=nfs --opt o=nfsvers=4,addr=192.168.0.5,rw --opt device=:/docker/http_acme http_acme
 docker volume create --driver local --opt type=nfs --opt o=nfsvers=4,addr=192.168.0.5,rw --opt device=:/docker/http_certificates http_certificates
-docker volume create --driver local --opt type=nfs --opt o=nfsvers=4,addr=192.168.0.5,rw --opt device=:/docker/http_passwd http_passwd
 # todo remove
 docker volume create --driver local --opt type=nfs --opt o=nfsvers=4,addr=192.168.0.5,rw --opt device=:/docker/wireguard_wg-access-server-data wireguard_wg-access-server-data
 docker volume create --driver local --opt type=nfs --opt o=nfsvers=4,addr=192.168.0.5,rw --opt device=:/ data
