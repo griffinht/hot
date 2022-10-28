1abfaf9e45fc6b4df778f0b40281888bc6b0c0b5
griffinht <git@griffinht.com>
2022-10-28 16:44:17 -0400

    fix ssh key persistence overriding

--- ci/git/bin/docker-entrypoint.sh
+++ ci/git/bin/docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
-ssh-keygen -f /ssh/ssh_host_id_ed25519_key -N '' -t ed25519
+if test -n /ssh/ssh_host_id_ed25519_key; then
+    ssh-keygen -f /ssh/ssh_host_id_ed25519_key -N '' -t ed25519
+else
+    echo found existing ssh key, not regenerating
+fi
 
 exec /usr/sbin/sshd -D
