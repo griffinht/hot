9382aabf79227d84620272358e365bd13832d209
griffinht <git@griffinht.com>
2023-01-08 18:09:50 -0500

    scribe is broken

--- http/proxied/scribe/build.sh
+++ http/proxied/scribe/build.sh
@@ -3,14 +3,18 @@
 DIR="$(mktemp -d)"
 cd "$DIR" || exit 1
 
-GIT_REF='3c6c4770d0d57d7e9ce341dfb850de90ffc429c0'
-SHA256='126d81bbb6968fcdae525c041d0a5f4b985f2195c0d0cdcab83e31f3700a76c6  tar.gz'
+#todo broken
+
+GIT_REF='761e4ef170da87a36160a024e1cf1a1275751b56'
+SHA256='326e77a96ef586174d2c00bdf4b76dc812a1bec8e4cc0e4271915c95a722ed85  tar.gz'
 
 if ! (curl "https://git.sr.ht/~edwardloveall/scribe/archive/$GIT_REF.tar.gz" > tar.gz \
     && echo "$SHA256" | sha256sum -c \
     && tar -xz --strip-components=1 < tar.gz \
     && rm tar.gz); then
-    echo failed to download and verify source
+    echo failed to download and verify source, 
+    echo "$SHA256" given, actual:
+    sha256sum tar.gz
     exit 1
 fi
 
