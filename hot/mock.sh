40c887919325afa8385054f59fe857f37b9b4b04
griffinht <git@griffinht.com>
2023-02-24 13:55:40 -0500

    add mock volume for hot

---
+++ hot/mock.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+docker volume create authelia
+docker volume create data
+docker volume create grafana
+docker volume create grocy
+docker volume create invidious
+docker volume create photoprism
+docker volume create media
+docker volume create miniflux
+docker volume create vaultwarden
