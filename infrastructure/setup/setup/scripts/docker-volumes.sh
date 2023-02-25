e72a72e298dd220e31aae9667f6a77cb4792aff9
griffinht <git@griffinht.com>
2023-02-25 10:16:08 -0500

    fix docker volumes for certs

--- infrastructure/setup/setup/scripts/docker-volumes.sh
+++ infrastructure/setup/setup/scripts/docker-volumes.sh
@@ -3,7 +3,7 @@
 # todo remove prefix http_
 #docker volume create --driver local --opt type=nfs --opt o=nfsvers=4,addr=192.168.0.5,rw --opt device=:/docker/http_acme http_acme
 docker volume create --driver local --opt type=nfs --opt o=nfsvers=4,addr=192.168.0.5,rw --opt device=:/docker/https_lego https_lego
-docker volume create --driver local --opt type=nfs --opt o=nfsvers=4,addr=192.168.0.5,rw --opt device=:/docker/https_certificates https_certificates
+docker volume create --driver local --opt type=nfs --opt o=nfsvers=4,addr=192.168.0.5,rw --opt device=:/docker/https_lego/certificates https_certificates
 # todo remove
 #docker volume create --driver local --opt type=nfs --opt o=nfsvers=4,addr=192.168.0.5,rw --opt device=:/docker/wireguard_wg-access-server-data wireguard_wg-access-server-data
 docker volume create --driver local --opt type=nfs --opt o=nfsvers=4,addr=192.168.0.5,rw --opt device=:/ data
