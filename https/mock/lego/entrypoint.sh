b663fd1811db7cec299142bf29678201a51aec20
griffinht <git@griffinht.com>
2023-02-25 15:17:27 -0500

    fix mock lego

--- https/mock/lego/entrypoint.sh
+++ https/mock/lego/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
+cd /lego
+mkdir certificates
 cd certificates
-mkdir griffinht.com
-cd griffinht.com
 
-openssl req -new -x509 -nodes -sha256 -nodes -subj "/CN=localhost" -keyout griffinht.com.key > fullchain.cer
-#openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365 -nodes -subj "/CN=localhost"
-
-tail -f /dev/null
+if [ ! -f griffinht.com.crt ]; then
+    openssl req -new -x509 -nodes -sha256 -nodes -subj "/CN=localhost" -keyout griffinht.com.key > griffinht.com.crt
+    #openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365 -nodes -subj "/CN=localhost"
+fi
