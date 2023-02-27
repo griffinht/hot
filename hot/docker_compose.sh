9cb5f052f4f0420a704875b3218265e78ae7366e
griffinht <git@griffinht.com>
2023-02-27 16:16:32 -0500

    improve test script

--- hot/docker_compose.sh
+++ hot/docker_compose.sh
@@ -5,10 +5,23 @@ EXIT=0
 for dir in proxied/*; do
     (
     cd $dir
-    if ! docker compose $@; then
-        EXIt=1
+    
+    if [ -n "$PRODUCTION" ]; then
+        if [ -f production.env ]; then
+            source ./production.env
+        else
+            export COMPOSE_FILE=docker-compose.yml
+        fi
     fi
-    )
+
+    if ! docker compose $@; then
+        echo "docker compose $@ exited with code $?"
+        echo ======================================
+        exit 1
+    fi
+    ) || EXIT=1
 done
 
-exit $EXIT
+if [ "$EXIT" -ne 0 ]; then echo ===================== non zero exit code; fi
+
+exit "$EXIT"
