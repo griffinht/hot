82df3337673c1c1955b031dfa3ec47e5a43dbc14
griffinht <git@griffinht.com>
2023-06-23 10:23:20 -0400

    fix mock lego directorys not working

--- https/mock/lego/entrypoint.sh
+++ https/mock/lego/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env sh
 
-cd /lego
-mkdir certificates
-cd certificates
-
 if [ ! -f griffinht.com.crt ]; then
     openssl req -new -x509 -nodes -sha256 -nodes -subj "/CN=localhost" -keyout griffinht.com.key > griffinht.com.crt
     #openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365 -nodes -subj "/CN=localhost"
