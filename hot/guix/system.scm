d8858b67796fad5e652df7c3d1f35d7db5a6cb19
griffinht <git@griffinht.com>
2023-07-31 10:49:57 -0400

    start to add test guix nginx

---
+++ hot/guix/system.scm
@@ -0,0 +1,16 @@
+(use-modules (gnu))
+(use-package-modules curl certs)
+(use-service-modules web)
+;; todo is there a more terse way to write this? like ;(use-package-modules bootloaders ssh)
+(operating-system
+  (host-name "envy-laptop")
+  (file-systems %base-file-systems)
+  (bootloader (bootloader-configuration
+                ; todo grub-efi-bootloader?
+                (bootloader grub-bootloader)))
+  ;todo remove timezone not necessary? defaults are fine?
+  (packages (append (list nss-certs ; tls certs from mozilla, required for https to work
+                          curl) ; helpful for occasional debugging
+                    %base-packages))
+  (services (append (list (service nginx-service-type))
+                    %base-services)))
