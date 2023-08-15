ce403cceebe9ea68d387a02aebe510c37a345c92
griffinht <git@griffinht.com>
2023-08-15 09:34:44 -0400

    change envy-laptop to dhcp, fix pubkey

--- infrastructure/envy-laptop/guix/system.scm
+++ infrastructure/envy-laptop/guix/system.scm
@@ -70,21 +70,29 @@
                                  )
                            %base-packages))
                  (services
-                  (append (list ;(service dhcp-client-service-type)
-                                (service static-networking-service-type
-                                         (list (static-networking
-                                                (addresses
-                                                 (list (network-address
-                                                        (device "eno1")
-                                                        (value "192.168.0.6/24"))))
-                                                (routes
-                                                 (list (network-route
-                                                        (destination "default")
-                                                        (gateway "192.168.0.1"))))
-                                                (name-servers '("192.168.0.1")))))
+                  (append (list (service dhcp-client-service-type)
+                                ; CURRENTLY BROKEN
+                                ; https://issues.guix.gnu.org/64653
+                                ; workaround is to use dhcp then switch to static-networking then switch back!?!?!
+                                ; WORKAROUND: ip address add 192.168.0.6/24 dev eno1
+                                ; basically static-networking breaks things on boot/restart of networking service
+                                ; so use dhcp then redeploy with static networking then go back to dhcp
+                                ; then the server magically listens from both interfaces lol i guess static networking doesn't clean up after itself
+                                ;(service static-networking-service-type
+                                ;         (list (static-networking
+                                ;                (addresses
+                                ;                 (list (network-address
+                                ;                        (device "eno1")
+                                ;                        (value "192.168.0.6/24"))))
+                                ;                (routes
+                                ;                 (list (network-route
+                                ;                        (destination "default")
+                                ;                        (gateway "192.168.0.1"))))
+                                ;                (name-servers '("192.168.0.1"))))) ; todo what if i don't specify name servers?
                                 ; unattended upgrades - why not i suppose todo hopefully this doesn't break anything
                                 ; https://guix.gnu.org/manual/en/html_node/Unattended-Upgrades.html
-                                (service unattended-upgrade-service-type)
+                                ; todo disable
+                                ;(service unattended-upgrade-service-type)
                                 (service openssh-service-type
                                          (openssh-configuration
                                           (openssh openssh-sans-x)
@@ -111,7 +119,7 @@
                                              (list
                                                (wireguard-peer
                                                  (name "smart-laptop")
-                                                 (public-key "X+xOySbHLKpA5j4Wo0Aw+VjeJOVruaSWLnknRwsrGAg=")
+                                                 (public-key "V//LVPk6jLy6tQWxIyqDapeP7kj2bZ84YAsmCoigdQ4=")
                                                  (allowed-ips '("10.0.0.2/32")))
                                                (wireguard-peer
                                                  (name "phone")
