d9b9722a7e7783cc73b4a6d8f81c513610f84ddd
griffinht <git@griffinht.com>
2023-08-15 20:10:53 -0400

    mikrotik static local dns, dyanmic ip via dns

--- infrastructure/mikrotik/mikrotik.sh
+++ infrastructure/mikrotik/mikrotik.sh
@@ -1,33 +1,37 @@
 #!/usr/bin/env sh
 
-#ssh user@192.168.0.1
-
-HOST=192.168.0.6
-EXTERNAL_ADDRESS=98.21.42.147
-# todo ansible??? probably more trouble than worth
-#https://stackoverflow.com/questions/68846248/how-can-i-add-my-ssh-public-key-to-a-mikrotik-router-device-using-the-command-li
-#todo disable winbox roman telnet
-#todo make sure ssh password login is disabled
+#https://wiki.mikrotik.com/wiki/Manual:Securing_Your_Router
+#https://libreddit.hot.griffinht.com/r/mikrotik/comments/pflvc2/how_do_i_reserve_a_specific_static_ip_for_a_device/
 
 cat << EOF
-# http with nat hairpin
+# leave only ssh
+/ip service disable telnet,ftp,www,www-ssl,api,winbox,api-ssl
+
+# static local dns
+/ip dns static add name=lil-tik.lan.griffinht.com address=192.168.0.1
+/ip dns static add name=hot-desktop.lan.griffinht.com address=192.168.0.5
+/ip dns static add name=envy-laptop.lan.griffinht.com address=192.168.0.6
+#todo static dhcp lease
+
+# dynamic dns for hairpin nat external ip
+/ip firewall address-list add address=windy.griffinht.com list=WANIP
+
+# hairpin nat
 #https://help.mikrotik.com/docs/display/ROS/NAT#NAT-HairpinNAT
 #https://forum.mikrotik.com/viewtopic.php?t=172380
-ip firewall nat add action=masquerade chain=srcnat dst-address=$HOST out-interface-list=LAN protocol=tcp src-address=192.168.0.0/24
+/ip firewall nat add action=masquerade chain=srcnat dst-address=192.168.0.6 out-interface-list=LAN protocol=tcp src-address=192.168.0.0/24
+
 # nginx
-ip firewall nat add chain=dstnat action=dst-nat dst-address=$EXTERNAL_ADDRESS dst-port=80 to-addresses=$HOST protocol=tcp
-ip firewall nat add chain=dstnat action=dst-nat dst-address=$EXTERNAL_ADDRESS dst-port=443 to-addresses=$HOST protocol=tcp
-
+/ip firewall nat add chain=dstnat action=dst-nat dst-address-list=WANIP dst-port=80 to-addresses=192.168.0.6 protocol=tcp
+/ip firewall nat add chain=dstnat action=dst-nat dst-address-list=WANIP dst-port=443 to-addresses=192.168.0.6 protocol=tcp
 # wireguard
-ip firewall nat add chain=dstnat action=dst-nat dst-address=$EXTERNAL_ADDRESS dst-port=51820 to-addresses=$HOST protocol=udp
+/ip firewall nat add chain=dstnat action=dst-nat dst-address-list=WANIP dst-port=51820 to-addresses=192.168.0.6 protocol=udp
 
-# smtp
-ip firewall nat add chain=dstnat action=dst-nat dst-address=$EXTERNAL_ADDRESS dst-port=25 to-addresses=$HOST protocol=tcp
+
+
+
 EOF
 
-exit
-#!/bin/bash
 
-# https://forum.mikrotik.com/viewtopic.php?t=94355
-ssh-keygen -if mikrotik_rsa.pub -m PKCS8 | ssh-keygen -lf -
-
+#away
+#ip dns static add name=big-tik.lan.griffinht.com address=192.168.0.1
