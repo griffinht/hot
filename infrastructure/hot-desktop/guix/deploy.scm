09fd3aa183051b237987bd6a7b4d508c606edb43
griffinht <git@griffinht.com>
2023-12-24 06:20:46 -0500

    hot-desktop update deploy

--- infrastructure/hot-desktop/guix/deploy.scm
+++ infrastructure/hot-desktop/guix/deploy.scm
@@ -16,20 +16,11 @@
                          ;; what user to login as, default root
                          (user "root")
                          ;; ssh public key of remote system
-                         ;; note guix will use the host machine's known_hosts file
-                         ;; guix also will not interactively prompt you to trust the new key
-                         ;; thats why if you don't want to add to known_hosts?? does it add anyway
-                         ;; then explicitly provide the identity file
-                         ;; its easier to omit and use ~/.ssh/known_hosts
-                         ;; just log in via ssh on the host machine to trust the key, then guix deploy
-                         ;; actually this is deprecated and bad
-                         ;; the right thing is to log in via ssh and then add the fingerprint
-                         ;; todo configure via .env? shouldn't really be in vcs... actually maybe it should
                          ;; obtain with `ssh-keyscan -p 2222 hostname`
-                         (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILVNdmLV0QcqU8Z4apBeO58eWLYq2BPlvNzDiZs999Ve")
+                         (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGOay9Mz5klPe0VdJRxzCx1juclU0TtEJOFc4Ieqt9Po")
                          ;; private key of host machine to authenticate with
                          ;; leave default its fine i think
                          ;;(identity "id_ed25519.pub")
-                         ;; ssh server port, default 22
+                         ;; ssh server port of remote system, default 22
                          ;(port 2222)))))
                          (port 22)))))
