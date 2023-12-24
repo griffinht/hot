e27026e428e2ef0cc588e3e9a09bb6bf2f2eb795
griffinht <git@griffinht.com>
2023-12-23 21:53:04 -0500

    update network config

--- infrastructure/mikrotik/mikrotik.sh
+++ infrastructure/mikrotik/mikrotik.sh
@@ -14,14 +14,10 @@ cat << EOF
 /ip dns static add name=hot-desktop.lan.griffinht.com address=192.168.0.5
 /ip dns static add name=envy-laptop.lan.griffinht.com address=192.168.0.6
 
-#something like this
-#todo static dhcp lease
-#/ip dhcp-server lease make-static
-#
 #192.168.0.1 lil-tik static
 #192.168.0.2 fruit-pi static
 #/ip dhcp-server lease add address=192.168.0.3 mac-address=E4:11:5B:61:70:DD server=defconf
-#/ip dhcp-server lease add address=192.168.0.5 mac-address=fc:aa:14:b0:1b:64 server=defconf
+/ip dhcp-server lease add address=192.168.0.5 mac-address=fc:aa:14:b0:1b:64 server=defconf
 /ip dhcp-server lease add address=192.168.0.6 mac-address=E4:11:5B:61:70:DD server=defconf
 
 # dynamic dns for hairpin nat external ip
@@ -30,15 +26,17 @@ cat << EOF
 # hairpin nat
 #https://help.mikrotik.com/docs/display/ROS/NAT#NAT-HairpinNAT
 #https://forum.mikrotik.com/viewtopic.php?t=172380
-/ip firewall nat add action=masquerade chain=srcnat dst-address=192.168.0.6 out-interface-list=LAN protocol=tcp src-address=192.168.0.0/24
+/ip firewall nat add action=masquerade chain=srcnat dst-address=192.168.0.5 out-interface-list=LAN protocol=tcp src-address=192.168.0.0/24
+#would need to be udp for wg????
+#/ip firewall nat add action=masquerade chain=srcnat dst-address=192.168.0.6 out-interface-list=LAN protocol=tcp src-address=192.168.0.0/24
 
 # nginx
-/ip firewall nat add chain=dstnat action=dst-nat dst-address-list=WANIP dst-port=80 to-addresses=192.168.0.6 protocol=tcp
-/ip firewall nat add chain=dstnat action=dst-nat dst-address-list=WANIP dst-port=443 to-addresses=192.168.0.6 protocol=tcp
+/ip firewall nat add chain=dstnat action=dst-nat dst-address-list=WANIP dst-port=80 to-addresses=192.168.0.5 protocol=tcp
+/ip firewall nat add chain=dstnat action=dst-nat dst-address-list=WANIP dst-port=443 to-addresses=192.168.0.5 protocol=tcp
 # wireguard
-/ip firewall nat add chain=dstnat action=dst-nat dst-address-list=WANIP dst-port=51820 to-addresses=192.168.0.6 protocol=udp
+/ip firewall nat add chain=dstnat action=dst-nat dst-address-list=WANIP dst-port=51820 to-addresses=192.168.0.5 protocol=udp
 # wire2
-/ip firewall nat add chain=dstnat action=dst-nat dst-address-list=WANIP dst-port=51821 to-addresses=192.168.0.5 protocol=udp
+/ip firewall nat add chain=dstnat action=dst-nat dst-address-list=WANIP dst-port=51821 to-addresses=192.168.0.6 protocol=udp
 
 
 
