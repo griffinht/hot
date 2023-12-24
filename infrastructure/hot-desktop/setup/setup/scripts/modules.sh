1d416748fa160929c936efe171b6b30ec24e85a2
griffinht <git@griffinht.com>
2023-12-24 06:13:18 -0500

    remove old hot desktop

--- infrastructure/hot-desktop/setup/setup/scripts/modules.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-cat << EOF >> /etc/modules
-nfs
-nfsd
-EOF
--- infrastructure/hot-desktop/setup/setup/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ "$(id -u)" -ne 0 ]; then
-  echo 'cd '"$(pwd)"' && ./hot.sh' | sudo --login
-else
-  ./hot.sh
-fi
