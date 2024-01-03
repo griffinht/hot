6b80e1363a2892958b070b34832a179111057be0
griffinht <git@griffinht.com>
2024-01-02 19:40:39 -0500

    remove ci files

--- ci/git/bin/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-if test -n /ssh/ssh_host_id_ed25519_key; then
-    ssh-keygen -f /ssh/ssh_host_id_ed25519_key -N '' -t ed25519
-else
-    echo found existing ssh key, not regenerating
-fi
-
-exec /usr/sbin/sshd -D
