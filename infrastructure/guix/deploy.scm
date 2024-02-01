1740cc2003c9062fc04ec5e27b1b20f63323f56c
griffinht <git@griffinht.com>
2024-02-01 13:17:35 -0500

    refactor deploy

--- infrastructure/guix/deploy.scm
+++ infrastructure/guix/deploy.scm
@@ -3,7 +3,7 @@
 (list
   (machine
     ; 'managed-host is a machine that is already running the Guix system and available over the network (via ssh)
-    (operating-system (load "system.scm"))
+    (operating-system (load (getenv "SYSTEM")))
     (environment managed-host-environment-type)
     (configuration
       (local-machine-ssh-configuration))))
---
+++ infrastructure/guix/podmanrootless/system.scm
@@ -0,0 +1,62 @@
+(use-modules (gnu packages ssh)
+             (gnu packages certs)
+             (gnu packages admin)
+             (gnu packages docker)
+             (gnu services networking)
+             (gnu services ssh)
+             (gnu services sysctl)
+             (guix gexp)
+             (griffinht packages-bin docker)
+             (griffinht system))
+
+(operating-system
+  (host-name "podmanrootless")
+  (bootloader %vm-bootloader)
+  (file-systems %vm-file-systems)
+  (users
+    (append
+      (list
+        (user-account
+          (name "podman")
+          (group "users")))))
+  (packages
+    (append
+      (list
+        nss-certs
+        shadow ; newuidmap with setuid
+        podman)
+      %base-packages))
+  (services
+    (append
+      (list (service dhcp-client-service-type)
+            (service openssh-service-type
+                     (openssh-configuration
+                      (openssh openssh-sans-x)
+                      (permit-root-login `prohibit-password)
+                      (password-authentication? #f)
+                      (authorized-keys
+                       `(("root" ,(local-file "../id_ed25519.pub"))
+                         ("podman" ,(local-file "../id_ed25519.pub")))))) ))
+            ; [rootlesskit:parent] error: failed to setup UID/GID map: failed to compute uid/gid map: open /etc/subuid: no such file or directory
+            ; https://www.mail-archive.com/guix-devel@gnu.org/msg66974.html
+            #|
+            (simple-service
+              'etc-subuid etc-service-type
+              (list `("subuid" ,(plain-file "subuid"
+                                  (string-append "docker:100000:65536\n")))))
+            (simple-service
+              'etc-subgid etc-service-type
+              (list `("subgid" ,(plain-file "subgid"
+                                  (string-append "docker:100000:65536\n")))))
+            (service iptables-service-type
+                     (iptables-configuration)))|#
+      (modify-services
+        %vm-services
+        ;https://docs.docker.com/engine/security/rootless/#exposing-privileged-ports
+        ;https://issues.guix.gnu.org/61462
+        (sysctl-service-type
+          config =>
+          (sysctl-configuration
+            (settings
+              (append '(("net.ipv4.ip_unprivileged_port_start" . "0"))
+                      %default-sysctl-settings))))))))
