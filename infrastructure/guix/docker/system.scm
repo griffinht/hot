4dfa442b5ace62a421988bdc135e159970ec3de6
griffinht <git@griffinht.com>
2024-02-01 21:35:50 -0500

    docker: listen on tcp

--- infrastructure/guix/docker/system.scm
+++ infrastructure/guix/docker/system.scm
@@ -12,16 +12,19 @@
   (host-name "docker")
   (bootloader %vm-bootloader)
   (file-systems %vm-file-systems)
-  #|
   (packages
     (append
       (list
-        nss-certs
-        podman)
+        nss-certs)
+        ;podman)
       %base-packages))
-  |#
   (services
     (append
       (list (service dhcp-client-service-type)
-            (service docker-service-type))
+            (service docker-service-type)
+            ; /etc/docker must be writable by docker daemon
+            (extra-special-file "/etc/docker/daemon.json"
+                                (local-file "daemon.json"))
+            #|(simple-service 'bruh etc-service-type
+                            (list `("docker/daemon.json" ,(local-file "daemon.json"))))|#)
       %vm-services)))
