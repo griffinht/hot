8dece3851bf2608d4d5614f63658281b6befa82b
griffinht <git@griffinht.com>
2024-04-04 13:29:54 -0400

    status: make deploy script work for remote ssh

--- infrastructure/containers/status/deploy.sh
+++ infrastructure/containers/status/deploy.sh
@@ -3,18 +3,32 @@
 set -xe
 
 prefix=/home/griffin/.local/share/containers/storage/volumes/status_
+
+mkdir mnt
+sshfs root@nerd-vps.wg.hot.griffinht.com:/var/lib/docker/volumes mnt -o uid="$(id -u)" -o gid="$(id -g)"
+prefix=mnt/status_
+
+#sleep 100000
+
+cleanup() {
+    fusermount --unmount mnt
+    rmdir mnt
+}
+trap cleanup EXIT
+#trap cleanup INT
+
 suffix=/_data
 
 echo alertmanager
-cp alertmanager.yml "${prefix}alertmanager_config${suffix}"
+cp -r alertmanager_config/* "${prefix}alertmanager_config${suffix}"
 
 echo prometheus
-cp prometheus.yml "${prefix}prometheus_config${suffix}"
-docker compose kill --signal=SIGHUP prometheus
+cp -r prometheus_config/* "${prefix}prometheus_config${suffix}"
+#docker compose kill --signal=SIGHUP prometheus
 
 echo xmpp-alerts
-cp xmpp-alerts.yml "${prefix}prometheus_config${suffix}"
+cp -r xmpp-alerts_config/* "${prefix}xmpp-alerts_config${suffix}"
 
 echo caddy
-cp Caddyfile "${prefix}caddy_config${suffix}"
-cp -r html "${prefix}caddy_html${suffix}"
+cp -r caddy_config/* "${prefix}caddy_config${suffix}"
+cp -r caddy_html/* "${prefix}caddy_html${suffix}"
