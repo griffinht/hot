0e77c0ac50a05803d997b599893ddba5fb3090c1
griffinht <git@griffinht.com>
2024-04-04 15:37:24 -0400

    status: rework environment variables for local environment

--- infrastructure/containers/status/compose.sh
+++ infrastructure/containers/status/compose.sh
@@ -1,1 +1,1 @@
-DOCKER_HOST=ssh://root@nerd-vps.wg.hot.griffinht.com docker compose -f compose.yml "$@"
+DOCKER_HOST=ssh://root@nerd-vps.wg.hot.griffinht.com docker compose --env-file=production.env -f compose.yml -f compose.production.yml "$@"
