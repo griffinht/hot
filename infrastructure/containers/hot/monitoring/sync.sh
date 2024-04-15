8d54cc8df0e82a1cd6b133779458f7fb79fd7819
griffinht <git@griffinht.com>
2024-04-14 20:52:05 -0400

    monitoring: deploy alertmanager

--- infrastructure/containers/hot/monitoring/sync.sh
+++ infrastructure/containers/hot/monitoring/sync.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 scp -r prometheus_config/* cloudtest.lan.hot.griffinht.com:/var/lib/docker/volumes/monitoring_prometheus_config/_data
+scp -r alertmanager_config/* cloudtest.lan.hot.griffinht.com:/var/lib/docker/volumes/monitoring_alertmanager_config/_data
 #scp mktxp.conf cloudtest.lan.hot.griffinht.com:/var/lib/docker/volumes/monitoring_mktxp_config/_data
