4d3207579e005f6bc674a2b9c76fc14b8efe1157
griffinht <git@griffinht.com>
2024-04-24 13:41:36 -0400

    mikrotik fix config

--- infrastructure/mikrotik/mikrotik.sh
+++ infrastructure/mikrotik/mikrotik.sh
@@ -44,29 +44,24 @@ cat << EOF
 # hairpin nat
 #https://help.mikrotik.com/docs/display/ROS/NAT#NAT-HairpinNAT
 #https://forum.mikrotik.com/viewtopic.php?t=172380
-/ip firewall nat add action=masquerade chain=srcnat dst-address=192.168.0.5 out-interface-list=LAN protocol=tcp src-address=192.168.0.0/24
-#would need to be udp for wg????
-#/ip firewall nat add action=masquerade chain=srcnat dst-address=192.168.0.6 out-interface-list=LAN protocol=tcp src-address=192.168.0.0/24
 
-
-
-
+# masquerade fixes lan->external->lan thing
+# dstnat is just regular external->lan
 
 # hypervisor wireguard
+/ip firewall nat add action=masquerade chain=srcnat dst-address=192.168.0.5 out-interface-list=LAN src-address=192.168.0.0/24
 /ip firewall nat add chain=dstnat action=dst-nat dst-address-list=WANIP dst-port=51820 to-addresses=192.168.0.5 protocol=udp
 # mystuff-guix wireguard
+/ip firewall nat add action=masquerade chain=srcnat dst-address=192.168.0.8 out-interface-list=LAN src-address=192.168.0.0/24
 /ip firewall nat add chain=dstnat action=dst-nat dst-address-list=WANIP dst-port=51821 to-addresses=192.168.0.8 protocol=udp
 # podmanrootless nginx
+/ip firewall nat add action=masquerade chain=srcnat dst-address=192.168.0.9 out-interface-list=LAN src-address=192.168.0.0/24
 /ip firewall nat add chain=dstnat action=dst-nat dst-address-list=WANIP dst-port=80 to-addresses=192.168.0.9 protocol=tcp
 /ip firewall nat add chain=dstnat action=dst-nat dst-address-list=WANIP dst-port=443 to-addresses=192.168.0.9 protocol=tcp
 
-
-
 # terarria laptop
-# todo remove this rule i don't think that it is necessary
-/ip firewall nat add action=masquerade chain=srcnat dst-address=192.168.0.7 out-interface-list=LAN protocol=tcp src-address=192.168.0.0/24
-# terraria
-/ip firewall nat add chain=dstnat action=dst-nat dst-address-list=WANIP dst-port=7777 to-addresses=192.168.0.7 protocol=tcp
+#/ip firewall nat add action=masquerade chain=srcnat dst-address=192.168.0.7 out-interface-list=LAN protocol=tcp src-address=192.168.0.0/24
+#/ip firewall nat add chain=dstnat action=dst-nat dst-address-list=WANIP dst-port=7777 to-addresses=192.168.0.7 protocol=tcp
 EOF
 
 
