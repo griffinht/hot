9ab29654008753159a56474272c5f4c0205d220c
griffinht <git@griffinht.com>
2024-05-07 14:47:53 -0400

    system: improve deploy script

---
+++ infrastructure/guix/system/deploy2.scm
@@ -0,0 +1,8 @@
+(list
+  (car (load "cloudtest/deploy.scm"))
+  (car (load "griffinht/deploy.scm"))
+  (car (load "guix/deploy.scm"))
+  (car (load "hot/deploy.scm"))
+  (car (load "hot-data/deploy.scm"))
+  (car (load "hypervisor/deploy.scm"))
+  (car (load "nerd-vps/deploy.scm")))
