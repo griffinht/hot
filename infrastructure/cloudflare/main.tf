e8c61c8884e237ca0264a0eb168d18b2c5079fb0
griffinht <git@griffinht.com>
2024-06-02 10:24:20 -0400

    cf tf magic

--- infrastructure/cloudflare/main.tf
+++ infrastructure/cloudflare/main.tf
@@ -21,6 +21,14 @@ variable "cloudflare_api_token" {
     sensitive = true
 }
 
+locals {
+    gcp_public_ips = {
+      "appliku" = "35.237.190.125"
+      "cloud66" = "34.73.180.83"
+      "tailscale" = "34.73.227.219"
+    }
+}
+
 # todo do this
 variable "records" {
     type = map(object({
@@ -34,53 +42,45 @@ variable "records" {
             value = "67.140.90.146"
             type = "A"
         }
+        hot = {
+            name = "hot"
+            value = "windy.griffinht.com"
+            type = "CNAME"
+        }
+        hot_all = {
+            name = "*.hot"
+            value = "hot.griffinht.com"
+            type = "CNAME"
+        }
+        cool = {
+            name = "cool"
+            value = "cool.tail773884.ts.net"
+            type = "CNAME"
+        }
+        cool_all = {
+            name = "*.cool"
+            value = "cool.griffinht.com"
+            type = "CNAME"
+        }
     }
 }
 
+locals {
+    computed_records = merge(var.records, {
+        for name, value in local.gcp_public_ips :
+        name => {
+            name = "${name}.gcp"
+            value = value
+            type = "A"
+        }
+    })
+}
+
 resource "cloudflare_record" "record" {
-    for_each = var.records
+    for_each = local.computed_records
     zone_id = "d691921860e35a45bc7f99007af14a7d"
     name = each.value.name
     value = each.value.value
     type = each.value.type
-    comment = "auto managed by terraform"
-}
-
-resource "cloudflare_record" "hot" {
-    zone_id = "d691921860e35a45bc7f99007af14a7d"
-    name = "hot"
-    value = "windy.griffinht.com"
-    type = "CNAME"
-    comment = "auto managed by terraform"
-}
-
-resource "cloudflare_record" "hot_all" {
-    zone_id = "d691921860e35a45bc7f99007af14a7d"
-    name = "*.hot"
-    value = "hot.griffinht.com"
-    type = "CNAME"
-    comment = "auto managed by terraform"
-}
-
-resource "cloudflare_record" "gcpdeb" {
-    zone_id = "d691921860e35a45bc7f99007af14a7d"
-    name = "gcpdeb"
-    value = "34.23.115.253"
-    type = "A"
-    comment = "auto managed by terraform"
-}
-
-resource "cloudflare_record" "cool" {
-    zone_id = "d691921860e35a45bc7f99007af14a7d"
-    name = "cool"
-    value = "cool.tail773884.ts.net"
-    type = "CNAME"
-    comment = "auto managed by terraform"
-}
-resource "cloudflare_record" "cool_all" {
-    zone_id = "d691921860e35a45bc7f99007af14a7d"
-    name = "*.cool"
-    value = "cool.griffinht.com"
-    type = "CNAME"
     comment = "auto managed by terraform"
 }
