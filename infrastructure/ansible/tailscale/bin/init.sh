54269af8996b416124bd09e03269cc2ba263ef09
griffinht <git@griffinht.com>
2024-06-02 13:45:37 -0400

    bruh

--- infrastructure/ansible/tailscale/bin/init.sh
+++ infrastructure/ansible/tailscale/bin/init.sh
@@ -2,3 +2,5 @@
 
 # https://tailscale.com/kb/1147/cloud-gce#step-4-add-gce-dns-for-your-tailnet
 ssh.sh tailscale up --advertise-routes="${IPV4?}" --accept-dns=true
+echo go to tailscale https://login.tailscale.com/admin/machines and accept the subnet routes???
+echo 'then sudo tailscale up --accept-routes on client machines like ur dev machine'
