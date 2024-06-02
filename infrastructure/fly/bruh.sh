fc90e88678f1b453746cfc7c57169b6585be11a1
griffinht <git@griffinht.com>
2024-06-02 18:11:06 -0400

    fly stuff

--- infrastructure/fly/bruh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-shell.sh . -- flyctl wireguard create fly.conf
-scp fly.conf hotter.griffinht.com:/etc/wireguard
-ssh hotter.griffinht.com sh -c 'systemctl enable wg-quick@fly.service && systemctl start wg-quick@fly.service'
