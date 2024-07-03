24b11fef6d42cba821e684e0d65007f21a676824
griffinht <git@griffinht.com>
2024-07-02 21:50:32 -0400

    caddy fix stuff

--- infrastructure/compose/compose/caddy/bin/sync.sh
+++ infrastructure/compose/compose/caddy/bin/sync.sh
@@ -2,9 +2,12 @@
 
 set -xe
 
-echo todo quit sync
-read -r bruh
-if [ -n "${SSH_HOST+x}" ]; then
-    scp -r caddy_config/* "${SSH_HOST?}:/var/lib/docker/volumes/caddy_caddy_config/_data"
-fi
+bruh() {
+    echo todo quit sync
+    read -r bruh
+    if [ -n "${SSH_HOST+x}" ]; then
+        scp -r caddy_config/* "${SSH_HOST?}:/var/lib/docker/volumes/caddy_caddy_config/_data"
+    fi
+}
+docker compose cp caddy_config/Caddyfile caddy:/etc/caddy/Caddyfile
 docker compose exec caddy caddy reload --config /etc/caddy/Caddyfile
