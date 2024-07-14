e6c19b58c7a168653edd59858ae4b296770a85c8
griffinht <git@griffinht.com>
2024-07-14 15:29:38 -0400

    prom traefik

--- infrastructure/compose/compose/monitoring2/bin/sync.sh
+++ infrastructure/compose/compose/monitoring2/bin/sync.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-scp -r prometheus_config/* "$SSH_HOST":/var/lib/docker/volumes/monitoring2_prometheus_config/_data
+#scp -r prometheus_config/* "$SSH_HOST":/var/lib/docker/volumes/monitoring2_prometheus_config/_data
+docker compose cp prometheus_config/* prometheus:/config
 #scp -r alertmanager_config/* cloudtest.lan.hot.griffinht.com:/var/lib/docker/volumes/monitoring_alertmanager_config/_data
 #scp mktxp.conf cloudtest.lan.hot.griffinht.com:/var/lib/docker/volumes/monitoring_mktxp_config/_data
 #todo kill --signal=SIGHUP prometheus
